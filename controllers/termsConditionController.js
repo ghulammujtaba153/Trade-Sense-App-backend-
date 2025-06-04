4a4922de72da0ff4d584fedd085eff07c7736492
Mujtaba12-al <mujtaba@mativeinc.com>
2025-06-04 19:02:12 +0500

    commit

--- controllers/termsConditionController.js
+++ controllers/termsConditionController.js
@@ -2,12 +2,7 @@ import TermsCondition from "../models/termsConditionSchema.js";
 
 export const createTermsCondition = async (req, res) => {
     try {
-        const termsCondition = await TermsCondition.findOne({});
-        if (termsCondition) {
-            await TermsCondition.findByIdAndUpdate(termsCondition._id, req.body, { new: true });
-        } else {
-            const termsCondition = await TermsCondition.create(req.body);
-        }
+        const termsCondition = await TermsCondition.create(req.body);
         res.status(200).json({ message: "Terms and Condition created successfully" });
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -17,7 +12,28 @@ export const createTermsCondition = async (req, res) => {
 
 export const getTermsCondition = async (req, res) => {
     try {
-        const termsCondition = await TermsCondition.findOne({});
+        const termsCondition = await TermsCondition.find({});
+        res.status(200).json(termsCondition);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
+export const updateTermsCondition = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const termsCondition = await TermsCondition.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json(termsCondition);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const deleteTermsCondition = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const termsCondition = await TermsCondition.findByIdAndDelete(id);
         res.status(200).json(termsCondition);
     } catch (error) {
         res.status(500).json({ message: error.message });
--- routes/termsConditionRoutes.js
+++ routes/termsConditionRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createTermsCondition, getTermsCondition } from "../controllers/termsConditionController.js";
+import { createTermsCondition, deleteTermsCondition, getTermsCondition, updateTermsCondition } from "../controllers/termsConditionController.js";
 
 const termsRouter = express.Router();
 
 termsRouter.post("/", createTermsCondition);
 termsRouter.get("/", getTermsCondition);
+termsRouter.put("/:id", updateTermsCondition);
+termsRouter.delete("/:id", deleteTermsCondition);
 
 export default termsRouter;
